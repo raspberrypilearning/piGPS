7279527f882da0f26e0d999ffcc1cfd9c6bbc522
James Robinson <james.robinson@raspberrypi.org>
2017-02-03 14:21:09 +0000

    Update piGPS.py

--- piGPS.py
+++ piGPS.py
@@ -87,6 +87,22 @@ class GPS(object):
         return (float(d) + float(m) / 60)*sign
 
 
+    
+    def distanceToTarget(self,target):
+        if self.sat > 4:
+            # convert decimal degrees to radians 
+            lat1,lon1,lat2,lon2 = map(radians, [self.lat, self.lon, target[0],target[1]])
+
+            # haversine formula 
+            dlon = lon2 - lon1
+            dlat = lat2 - lat1
+            a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+            c = 2 * asin(sqrt(a))
+            r = 6371 # Radius of earth in kilometers. Use 3956 for miles
+            return c * r
+        else:
+            return None
+        
     def parseGGA(self,ggaString):
         rawList = ggaString.split(",")
         print(rawList[1])
