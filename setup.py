ab0b00a9f564808b090350cf2509bbb7c0949604
Laura <laura.sach@raspberrypi.org>
2017-07-31 10:57:23 +0100

    Add licence file and setup.py

---
+++ setup.py
@@ -0,0 +1,67 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='PiGPS',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.1.0',
+
+    description='GPS library for consuming location data from serial interface',
+    long_description='GPS library for consuming location data from serial interface'
+
+    # The project's main homepage.
+    url='https://github.com/raspberrypilearning/piGPS',
+
+    # Author details
+    author='James Robinson',
+    author_email='james.robinson@raspberrypi.org',
+
+    # Choose your license
+    license='BSD',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
+        "Topic :: Education",
+        "Topic :: Scientific/Engineering :: GIS",
+        "Intended Audience :: Education",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+    ],
+
+    # What does your project relate to?
+    keywords='gps mapping',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['pyserial'],
+
+
+)
